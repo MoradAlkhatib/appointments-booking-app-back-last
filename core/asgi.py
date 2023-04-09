5d05e59d90cd1a0fb56caa87c66c81b1727ee681
moradalkhatib <muradalkhateb@icloud.com>
2023-04-09 22:57:36 +0300

    change project name to be core

--- core/asgi.py
+++ core/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for appoitments_booking_app project.
+ASGI config for core project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'appoitments_booking_app.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
 application = get_asgi_application()
